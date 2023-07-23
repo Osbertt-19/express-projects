00798daee5103e088eb92ca199fdf97c8fc02837
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-07-23 17:42:00 +0630

    made getTaskByID, updateTask, deleteTask apis

--- task-manager/src/controllers/tasks.ts
+++ task-manager/src/controllers/tasks.ts
@@ -7,9 +7,30 @@ export const getAllTasks: RequestHandler = async (req, res) => {
   res.status(200).json({ tasks });
 };
 
+export const getTaskByID: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const gottenTask = await Task.findById(id).lean();
+  res.status(201).json({ gottenTask });
+};
+
 export const createTask: RequestHandler = async (req, res) => {
   const { name } = req.body;
-  console.log(req.body);
   const createdTask = await Task.create({ name });
   res.status(201).json({ createdTask });
+};
+
+export const updateTask: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const { name, completed } = req.body;
+  const updatedTask = await Task.findByIdAndUpdate(id, {
+    name,
+    completed,
+  }).lean();
+  res.status(201).json({ updatedTask });
+};
+
+export const deleteTask: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const deletedTask = await Task.findByIdAndDelete(id).lean();
+  res.status(201).json({ deletedTask });
 };
--- task-manager/src/routes/tasks.ts
+++ task-manager/src/routes/tasks.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { createTask, getAllTasks } from "../controllers/tasks";
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  getTaskByID,
+  updateTask,
+} from "../controllers/tasks";
 export const router = Router();
 
 router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTaskByID).patch(updateTask).delete(deleteTask);
+// router.route("/:id").get((req, res) => {
+//   const { id } = req.params;
+//   res.json({ name: id });
+// });
